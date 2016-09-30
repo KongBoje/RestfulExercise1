b657bed76038f7d5d61b21cea0eb57e6959aabc4
Michael Veilis <Michael Veilis>
2016-09-30 14:33:21 +0200

    first commit

---
+++ src/main/java/exception/GenericExceptionMapper.java
@@ -0,0 +1,31 @@
+package exception;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import javax.servlet.ServletContext;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+/**
+ *
+ * @author Thomas Hartmann - tha@cphbusiness.dk created on Sep 27, 2016
+ */
+@Provider
+public class GenericExceptionMapper implements ExceptionMapper<Exception> {
+
+    static Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    @Context
+    ServletContext context;
+
+    @Override
+    public Response toResponse(Exception exception) {
+        JsonObject job = new JsonObject();
+        job.addProperty("status", 404);
+        job.addProperty("msg", "The page/service you requested does not exist");
+        return Response.status(404).entity(gson.toJson(job)).build();
+    }
+}
